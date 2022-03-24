55463bde07d4d586165e3d0b5a95f052b14632ff
Tang <waltert3@illinois.edu>
2022-03-24 16:03:03 -0500

    preparing for terminal drivers

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -15,6 +15,21 @@
 
 #define SPECIAL_KEYCODES    0x81
 
+#define CAPSLOCK_PRESSED 0x3A
+#define CAPSLOCK_RELEASED 0xBA
+
+#define LSHIFT_PRESSED 0x2A
+#define LSHIFT_RELEASED 0xAA
+
+#define RSHIFT_PRESSED 0x36
+#define RSHIFT_RELEASED 0xB6
+
+#define LALT_PRESSED 0x38
+#define LALT_RELEASED 0xB8
+/*
+#define RALT_PRESSED 0xE0
+#define RALT_RELEASED 
+*/
 
 void keyboard_init(void);
 

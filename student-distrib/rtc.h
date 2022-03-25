66a47efd8cbae306ef3bf751e3ff936b848b3608
Hsu <sehsu2@illinois.edu>
2022-03-24 19:17:34 -0500

    header

--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -18,5 +18,10 @@
 
 extern void rtc_init(void);
 extern void rtc_handler(void);
+int32_t open (const uint8_t* filename);
+int32_t read (int32_t fd, void* buf, int32_t nbytes);
+int32_t write (int32_t fd, const void* buf, int32_t nbytes);
+int32_t close (int32_t fd);
+
 int testing_RTC ; 
 

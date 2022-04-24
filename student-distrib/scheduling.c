35460d9934e6c1fc96e05bc1cdfb5126153a1377
Hsu <sehsu2@illinois.edu>
2022-04-24 18:45:04 -0500

    init

--- student-distrib/scheduling.c
+++ student-distrib/scheduling.c
@@ -7,12 +7,12 @@
 #include "terminal.h"
 #include "x86_desc.h"
 
-volatile uint8_t cur = 0;
 volatile uint8_t prev = 0;
+volatile uint8_t cur = 0;
 
 extern void pit_init(void) {
     outb(FREQ_SET, MC_REG);
-    outb(PIT_FREQ & LOW_MASK, CHAN_0);
+    outb(PIT_FREQ && PIT_MASK, CHAN_0);
     outb(PIT_FREQ >> SHIFT, CHAN_0);
 
     enable_irq(PIT_IRQ_NUM);
@@ -20,6 +20,20 @@ extern void pit_init(void) {
 
 extern void pit_handler(void) {
     send_eoi(PIT_IRQ_NUM);
+    // if (prev == cur) 
+    //     scheduler(prev, cur);
+    scheduler();
+}
+
+extern void scheduler() {
     prev = cur;
-    
+    int i;
+    for (i = 0; i < MAX_TERM; i++) {
+        cur = (cur + 1) % MAX_TERM;
+    }
+    cont_switch();
+}
+
+extern void cont_switch() {
+
 }
--- student-distrib/scheduling.h
+++ student-distrib/scheduling.h
@@ -3,20 +3,20 @@
 #include "types.h"
 
 #define FREQ_SET        0x36
-#define CHAN_0          0x40
-#define MC_REG          0x43
-#define PIT_FREQ        11932
+#define CHAN_0          0x40  // PIT cmd port (channel 0 data port)
+#define MC_REG          0x43  // Mode/Command register
+#define PIT_FREQ        11932 // (1193180 + 50) / 100 
 
 #define PIT_IRQ_NUM     0
 #define SHIFT           8
-#define LOW_MASK        0xFF
+#define PIT_MASK        0xFF
 #define UP_MASK         0xFF00
+#define MAX_TERM        3
 
 
 extern void pit_init(void);
 extern void pit_handler(void);
-extern void cont_switch(int prev_id, int term_id);
-extern void init_terminals();
-extern void next_process();
+extern void scheduler();
+extern void cont_switch();
 
 #endif

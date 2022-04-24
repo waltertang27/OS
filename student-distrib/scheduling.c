3b799d09064135172eaf06634986238039092781
Hsu <sehsu2@illinois.edu>
2022-04-23 20:54:48 -0500

    init

--- student-distrib/scheduling.c
+++ student-distrib/scheduling.c
@@ -1,1 +1,25 @@
 #include "scheduling.h"
+#include "lib.h"
+#include "i8259.h"
+#include "systemcalls.h"
+#include "paging.h"
+#include "types.h"
+#include "terminal.h"
+#include "x86_desc.h"
+
+volatile uint8_t cur = 0;
+volatile uint8_t prev = 0;
+
+extern void pit_init(void) {
+    outb(FREQ_SET, MC_REG);
+    outb(PIT_FREQ & LOW_MASK, CHAN_0);
+    outb(PIT_FREQ >> SHIFT, CHAN_0);
+
+    enable_irq(PIT_IRQ_NUM);
+}
+
+extern void pit_handler(void) {
+    send_eoi(PIT_IRQ_NUM);
+    prev = cur;
+    
+}
--- student-distrib/scheduling.h
+++ student-distrib/scheduling.h
@@ -1,1 +1,22 @@
+#ifndef SCHEDULING_H
+#define SCHEDULING_H
+#include "types.h"
 
+#define FREQ_SET        0x36
+#define CHAN_0          0x40
+#define MC_REG          0x43
+#define PIT_FREQ        11932
+
+#define PIT_IRQ_NUM     0
+#define SHIFT           8
+#define LOW_MASK        0xFF
+#define UP_MASK         0xFF00
+
+
+extern void pit_init(void);
+extern void pit_handler(void);
+extern void cont_switch(int prev_id, int term_id);
+extern void init_terminals();
+extern void next_process();
+
+#endif

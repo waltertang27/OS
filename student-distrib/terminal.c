02e6da4830d8fb9134424b82c3d1a26c9cde7a77
Singh <singh94@illinois.edu>
2022-04-25 13:21:15 -0500

    hi walter

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -147,30 +147,23 @@ extern void switch_terminals(int32_t prevTerminal)
     clear(); 
     printf(" Switching from Terminal %d to Terminal %d \n", prevTerminal + 1, terminal_flag + 1); 
 
-    // Store page is where you are copying video memory too(Save what is in video memory to store page)
-    void * storePage = (((2+prevTerminal) * FOURKB)); 
-    //Movepage is the videomemory you are moving into video memory 
-    void * movePage = (((2+terminal_flag) * FOURKB)) ; 
+    // // Store page is where you are copying video memory too(Save what is in video memory to store page)
+    // void * storePage = (((2+prevTerminal) * FOURKB)); 
+    // //Movepage is the videomemory you are moving into video memory 
+    // void * movePage = (((2+terminal_flag) * FOURKB)) ; 
 
-    // ^ those both are def not right which is calusing the issue 
+    // // ^ those both are def not right which is calusing the issue 
 
-    // Move the 4kb Vid memory into a seperate page to preserve it 
-    memcpy(storePage,(void *)VID_ADDR,FOURKB);
+    // // Move the 4kb Vid memory into a seperate page to preserve it 
+    // memcpy(storePage,(void *)VID_ADDR,FOURKB);
 
-    // Check if you have opened this terminal if not, execute shell 
-    if(terminal_shell[terminal_flag] == 0){
-        terminal_shell[terminal_flag] == 1 ;
-        // Execute shell 
-    }
-    else{
-        //If it has already been opened, move its memory ito video memory(grey 4kb page)
-    memcpy((void *)VID_ADDR,movePage,FOURKB); 
-    }
-
-
-
-    // Check if its the first time that you are opening the terminal, if so boot shell 
-
-    // If not get its video page and move it to actual video memory 
-
+    // // Check if you have opened this terminal if not, execute shell 
+    // if(terminal_shell[terminal_flag] == 0){
+    //     terminal_shell[terminal_flag] == 1 ;
+    //     // Execute shell 
+    // }
+    // else{
+    //     //If it has already been opened, move its memory ito video memory(grey 4kb page)
+    // memcpy((void *)VID_ADDR,movePage,FOURKB); 
+    //}
 }

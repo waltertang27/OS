e584599339c9a21974e9dcf6e0b4583e0a46b6b0
Kevin Lim <wzlim2@illinois.edu>
2022-04-26 00:36:09 -0500

    paging in scheduling

--- student-distrib/scheduling.c
+++ student-distrib/scheduling.c
@@ -67,11 +67,52 @@ extern void scheduler() {
     cur = (cur + 1);
     cur = cur % MAX_TERM;
 
+    /* setup video memory (from vidmap) */
+    // setup page directory entry
+    page_directory[VIDMAP_INDEX].user_supervisor = 1;
+    page_directory[VIDMAP_INDEX].present = 1;
+    page_directory[VIDMAP_INDEX].page_size = 0;
+    page_directory[VIDMAP_INDEX].read_write = 1;
+    page_directory[VIDMAP_INDEX].write_through = 0;
+    page_directory[VIDMAP_INDEX].cache_disable = 0;
+    page_directory[VIDMAP_INDEX].accessed = 0;
+    page_directory[VIDMAP_INDEX].dirty = 0;
+    page_directory[VIDMAP_INDEX].page_table_addr = (int32_t)video_mapping_pt / ALIGN_BYTES; // points to the video mapping page table
 
-    // more asm
+    // setup video mapping table entry
+    video_mapping_pt[0].user_supervisor = 1;
+    video_mapping_pt[0].present = 1;
+    video_mapping_pt[0].read_write = 1;
+    video_mapping_pt[0].write_through = 0;
+    video_mapping_pt[0].cache_disable = 1;
+    video_mapping_pt[0].accessed = 0;
+    video_mapping_pt[0].dirty = 0;
+    video_mapping_pt[0].attribute = 0;
+    video_mapping_pt[0].global = 0;
+    video_mapping_pt[0].page_table_addr = VID_ADDR / ALIGN_BYTES;
 
+    flush_tlb();
+
+    // store video memory
+    
     pcb = terminals[terminal_flag].currPCB;
 
+    // paging
+    page_directory[USER_INDEX].user_suprevisor = 1;
+    page_directory[USER_INDEX].present = 1;
+    page_directory[USER_INDEX].page_size = 1;
+    page_directory[USER_INDEX].read_write = 1;
+    page_directory[USER_INDEX].write_through = 0;
+	page_directory[USER_INDEX].cache_disable = 0;
+	page_directory[USER_INDEX].accessed = 0;
+	page_directory[USER_INDEX].dirty = 0;
+    page_directory[USER_INDEX].page_table_addr = (int32_t)(EIGHTMB + pcb->process_id * PAGE_SIZE) / ALIGN_BYTES;
+
+    flush_tlb();
+
+    tss.esp0 = EIGHTMB - SIZE_OF_INT32 - (EIGHTKB * pcb->process_id);
+    tss.ss0 = KERNEL_DS;
+    send_eoi(PIT_IRQ_NUM);
     cont_switch();
 }
 

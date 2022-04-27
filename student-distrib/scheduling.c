b0086fcf9709c12841de6ab1a32d94e864c57944
Kevin Lim <wzlim2@illinois.edu>
2022-04-26 21:55:54 -0500

    readd paging

--- student-distrib/scheduling.c
+++ student-distrib/scheduling.c
@@ -119,6 +119,20 @@ extern void scheduler() {
     
     pcb = (pcb_t *)terminals[terminal_flag].currPCB;
 
+    // paging
+    page_directory[USER_INDEX].user_suprevisor = 1;
+    page_directory[USER_INDEX].present = 1;
+    page_directory[USER_INDEX].page_size = 1;
+    page_directory[USER_INDEX].read_write = 1;
+    page_directory[USER_INDEX].write_through = 0;
+	page_directory[USER_INDEX].cache_disable = 0;
+	page_directory[USER_INDEX].accessed = 0;
+	page_directory[USER_INDEX].dirty = 0;
+    page_directory[USER_INDEX].page_table_addr = (int32_t)(EIGHTMB + pcb->process_id * PAGE_SIZE) / ALIGN_BYTES;
+
+    flush_tlb();
+
+
     tss.esp0 = EIGHTMB - SIZE_OF_INT32 - (EIGHTKB * pcb->process_id);
     tss.ss0 = KERNEL_DS;
     send_eoi(PIT_IRQ_NUM);

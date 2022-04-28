9f982aab56254112beaeae554c1566551bee8f7c
Kevin Lim <wzlim2@illinois.edu>
2022-04-27 20:52:13 -0500

    pcb not set

--- student-distrib/scheduling.c
+++ student-distrib/scheduling.c
@@ -15,14 +15,14 @@ extern void flush_tlb();
 
 extern void pit_init(void) {
     outb(FREQ_SET, MC_REG);
-    outb(PIT_FREQ && PIT_MASK, CHAN_0);
+    outb(PIT_FREQ & PIT_MASK, CHAN_0);
     outb(PIT_FREQ >> SHIFT, CHAN_0);
 
     enable_irq(PIT_IRQ_NUM);
 }
 
 extern void pit_handler(void) {
-    send_eoi(PIT_IRQ_NUM);
+    // send_eoi(PIT_IRQ_NUM);
     // if (prev == cur) 
     //     scheduler(prev, cur);
     scheduler();
